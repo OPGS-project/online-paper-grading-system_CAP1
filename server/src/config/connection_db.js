fba81492ba9c8499cd91c7bbe4b1660f62c399a8
anhduy9102 <anhduy9102@gmail.com>
2023-12-06 15:26:13 +0700

    fix get class by id_teacher

--- server/src/config/connection_db.js
+++ server/src/config/connection_db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require("sequelize");
 
 // Option 3: Passing parameters separately (other dialects)
-const sequelize = new Sequelize("cap1_opgs", "root", null, {
+const sequelize = new Sequelize("cap1_opgs", "root", "Anhduy5374?", {
   host: "localhost",
   dialect: "mysql",
   logging: false,
